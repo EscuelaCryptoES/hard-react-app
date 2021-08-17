5c4899e88af8b28dcd29641e08f80d402c989aec
ivanmmurciaua <imm117@alu.ua.es>
2021-08-17 15:30:50 +0200

    Tests OK && reduce frontend file complex

--- test/SimpleStorage.js
+++ test/SimpleStorage.js
@@ -6,18 +6,61 @@
 // We import Chai to use its asserting functions here.
 const { expect } = require("chai");
 
-const SimpleStorage = artifacts.require("./SimpleStorage.sol");
+// `describe` is a Mocha function that allows you to organize your tests. It's
+// not actually needed, but having your tests organized makes debugging them
+// easier. All Mocha functions are available in the global scope.
 
-contract("SimpleStorage", accounts => {
-  it("...should store the value 89.", async () => {
-    const simpleStorageInstance = await SimpleStorage.deployed();
+// `describe` recieves the name of a section of your test suite, and a callback.
+// The callback must define the tests of that section. This callback can't be
+// an async function.
+describe("Storage", function () {
+  // Mocha has four functions that let you hook into the the test runner's
+  // lifecyle. These are: `before`, `beforeEach`, `after`, `afterEach`.
 
-    // Set value of 89
-    await simpleStorageInstance.set(89, { from: accounts[0] });
+  // They're very useful to setup the environment for tests, and to clean it
+  // up after they run.
 
-    // Get stored value
-    const storedData = await simpleStorageInstance.get.call();
+  // A common pattern is to declare some variables, and assign them in the
+  // `before` and `beforeEach` callbacks.
 
-    assert.equal(storedData, 89, "The value 89 was not stored.");
+  let SimpleStorage;
+  let simpleStorageInstance;
+  let account;
+
+
+  // `beforeEach` will run before each test, re-deploying the contract every
+  // time. It receives a callback, which can be async.
+  beforeEach(async function () {
+    // Get the ContractFactory and Signers here.
+    SimpleStorage = await ethers.getContractFactory("SimpleStorage");
+    const[account] = await ethers.getSigners();
+
+    // To deploy our contract, we just have to call SimpleStorage.deploy() and await
+    // for it to be deployed(), which happens onces its transaction has been
+    // mined.
+    simpleStorageInstance = await SimpleStorage.deploy();
+    await simpleStorageInstance.deployed();
+
+    // We can interact with the contract by calling `simpleStorageInstance.method()`
+    await simpleStorageInstance.deployed();
+  });
+
+
+  // You can nest describe calls to create subsections.
+  describe("This storage test", function () {
+    // `it` is another Mocha function. This is the one you use to define your
+    // tests. It receives the test name, and a callback function.
+    it("...should store the value 89.", async () => {
+
+        const storedValue = 89;
+        
+        // Set value
+        await simpleStorageInstance.set(storedValue, { from: account });
+
+        // Get stored value
+        const storedData = await simpleStorageInstance.get.call();
+
+        expect(storedData).to.equal(storedValue);
+    });
   });
 });
