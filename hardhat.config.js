48cbf2a4a87703fbbd1683bbe53e08d572762f0a
ivanmmurciaua <imm117@alu.ua.es>
2021-10-11 13:29:36 +0200

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,10 @@
 require("@nomiclabs/hardhat-waffle");
 
+// The next line is part of the sample project, you don't need it in your
+// project. It imports a Hardhat task definition, that can be used for
+// testing the frontend.
+require("./tasks/faucet");
+
 module.exports = {
-networks: {
-  hardhat: {
-    chainId: 1337
-  }
-},
-solidity: "0.8.0"
+  solidity: "0.7.3"
 };
